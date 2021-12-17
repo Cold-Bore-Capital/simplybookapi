eb66949f7dabd07f8df740b7966222141e917106
Craig Lathrop <clathrop@coldborecapital.com>
2021-12-16 18:48:13 -0700

    Refactor to use unit testing on main token function.

--- simplybookapi/main.py
+++ simplybookapi/main.py
@@ -1,13 +1,12 @@
 import pickle
 from datetime import datetime, timedelta
-from typing import Dict, Any, Union, Callable, List
-
-from jsonrpcclient import Ok, parse, request
+from typing import Any, Union, Tuple
 
 import pandas as pd
 import requests
 from cbcdb import DBManager
 from configservice import Config
+from jsonrpcclient import request
 
 
 class Main:
@@ -22,6 +21,21 @@ class Main:
         self._access_token_cache = None
         # Start out with a now expire time
         self._access_token_cache_expire = datetime.now()
+        self._set_env_values(test_mode)
+
+    def _set_env_values(self, test_mode):
+        self._simply_book_api = Config(test_mode).get_env('SIMPLY_BOOK_URL',
+                                                          default_value='https://user-api.simplybook.me',
+                                                          test_response='https://abcdefg.com')
+
+        self._simply_book_company = Config(test_mode).get_env('COMPANY_NAME', default_value='petswellness',
+                                                              test_response='petswellness')
+
+        self._simply_book_api_key = Config(test_mode).get_env('SB_API_KEY', error_flag=True, test_response='ABCDEFG')
+
+        self._retry_limit = Config(test_mode).get_env('API_RETRY_LIMIT', default_value=3, data_type_convert='int')
+
+        self._token_expire = Config(test_mode).get_env('TOKEN_EXPIRE', default_value=10, data_type_convert='int')
 
     '''
     # Section - Public Methods
@@ -73,7 +87,7 @@ class Main:
                 return None
         return output
 
-    def get_auth_token(self, fail_counter: int = 0) -> dict:
+    def get_auth_token(self, api_path, company, api_key, fail_counter: int = 0) -> dict:
         """
         Requests an access token from the EzyVet API
 
@@ -86,33 +100,75 @@ class Main:
             is retrieved.
 
         """
-        simply_book_api = Config().get_env('SIMPLY_BOOK_URL', default_value='https://user-api.simplybook.me',
-                                           data_type_convert='str')
-        simply_book_company = Config().get_env('COMPANY_NAME', default_value='petswellness', data_type_convert='str')
-        simply_book_api_key = Config().get_env('SB_API_KEY', error_flag=True)
-        response = requests.post(url=f'{simply_book_api}/login',
-                                 json=request('getToken', params=(simply_book_company, simply_book_api_key)))
-
-        if response.status_code != 200:
-            if fail_counter > 3:
-                raise InvalidTokenResponse(response.text)
-
+        token = self._read_token_pickle(self._token_expire)
+        if not token:
+            path = f'{api_path}/login'
+            params = (company, api_key)
+            function = 'getToken'
+            token = self._sb_api_query(path, function, params)
+            if token:
+                current_dt = datetime.utcnow()
+                token_save = {'token': token,
+                              'create_date': current_dt}
+                with open('../token_save.pkl', 'wb') as token_file:
+                    pickle.dump(token_save, token_file)
+                return token
             else:
-                fail_counter += 1
-                self.get_auth_token(fail_counter)
-
-        data = response.json()
-        token = data['result']
-
-        if token:
-            current_dt = datetime.utcnow()
-            token_save = {'token': token,
-                          'create_date': current_dt}
-            with open('token_save.pkl', 'wb') as token_file:
-                pickle.dump(token_save, token_file)
-
+                raise EmptyTokenError()
+        else:
             return token
-        raise EmptyTokenError()
+
+    def _sb_api_query(self, path: str, function: str, params: Tuple[Any], fail_counter: int = 0) -> Union[dict, list]:
+        """
+        Queries the SB API and handles error retries.
+
+        Args:
+            path: The full path to the API end point. For example, https://user-api.simplybook.me/login
+            function: The name of the RPC function to call
+            params: A tuple containing the values to pass in to the RPC.
+            fail_counter: A counter to track the number of failed API query events.
+
+        Returns:
+            The decoded data from the API response as either a list or dict.
+        """
+        response = requests.post(url=path, json=request(function, params=params))
+        if response.status_code != 200:
+            if fail_counter > self._retry_limit:
+                raise InvalidTokenResponse(response.text)
+            else:
+                print(
+                    f'API query failed with error code {response.status_code}. Retry number {fail_counter}.\n{response.text}')
+                fail_counter += 1
+                self._sb_api_query(path, function, params, fail_counter)
+        data = response.json()
+        if 'error' in data.keys():
+            raise SBAPIError(data['error'])
+        else:
+            return data['result']
+
+    @staticmethod
+    def _read_token_pickle(token_expire_limit: int) -> Union[str, bool]:
+        """
+        Reads the locally saved token from pickle. Returns the token if not past expire time, otherwise returns false.
+
+        Args:
+            token_expire_limit: The number of minutes from token create date before the token becomes invalid.
+
+        Returns:
+            Either a token, or False.
+        """
+        try:
+            with open('../token_save.pkl', 'rb') as token_file:
+                token_data = pickle.load(token_file)
+                token_create_time = token_data['create_date']
+                now_time = datetime.utcnow()
+                token_age = (now_time - token_create_time).seconds / 60
+                if token_age > token_expire_limit:
+                    return False
+                else:
+                    return token_data['token']
+        except:
+            return False
 
 
 class InvalidTokenResponse(requests.exceptions.HTTPError):
@@ -120,4 +176,8 @@ class InvalidTokenResponse(requests.exceptions.HTTPError):
 
 
 class EmptyTokenError(Exception):
+    pass
+
+
+class SBAPIError(Exception):
     pass
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,90 @@
-from datetime import datetime
-import time
+from datetime import datetime, timedelta
+import time, pickle, os
 from unittest import TestCase
+from typing import Any, Union, Tuple
 from configservice import Config
 
 import pandas as pd
 
-from simplybookapi.main import Main
+from simplybookapi.main import Main, EmptyTokenError, InvalidTokenResponse
 
 
 class TestSimplyBookApi(TestCase):
 
+    def test__read_token_pickle(self):
+        # Make a pickle first
+        current_dt = datetime.utcnow()
+        token_save = {'token': 'abcdefg',
+                      'create_date': current_dt}
+        with open('../token_save.pkl', 'wb') as token_file:
+            pickle.dump(token_save, token_file)
+
+        m = Main(test_mode=True)
+        token = m._read_token_pickle(10)
+        golden = 'abcdefg'
+        self.assertEqual(golden, token)
+
+        current_dt = datetime.utcnow() - timedelta(minutes=12)
+        token_save = {'token': 'abcdefg',
+                      'create_date': current_dt}
+        with open('../token_save.pkl', 'wb') as token_file:
+            pickle.dump(token_save, token_file)
+
+        m = Main(test_mode=True)
+        token = m._read_token_pickle(10)
+        self.assertFalse(token)
+
+        os.remove('../token_save.pkl')
+        m = Main(test_mode=True)
+        token = m._read_token_pickle(10)
+        self.assertFalse(token)
+
+    def test_get_auth_token(self):
+        # Make a pickle first
+        current_dt = datetime.utcnow()
+        token_save = {'token': 'abcdefg',
+                      'create_date': current_dt}
+        with open('../token_save.pkl', 'wb') as token_file:
+            pickle.dump(token_save, token_file)
+
+        m = MockAuthToken(test_mode=True)
+        token = m.get_auth_token('abc', 'def', 'abc123')
+        golden = 'abcdefg'
+        self.assertEqual(golden, token)
+
+        current_dt = datetime.utcnow() - timedelta(minutes=12)
+        token_save = {'token': 'abcdefg',
+                      'create_date': current_dt}
+        with open('../token_save.pkl', 'wb') as token_file:
+            pickle.dump(token_save, token_file)
+
+        m = MockAuthToken(test_mode=True)
+        token = m.get_auth_token('abc', 'def', 'abc123')
+        golden = 'new_token_res'
+        self.assertEqual(golden, token)
+
+        os.remove('../token_save.pkl')
+        m = MockAuthToken(test_mode=True)
+        token = m.get_auth_token('abc', 'def', 'abc123')
+        golden = 'new_token_res'
+        self.assertEqual(golden, token)
+
+        with self.assertRaises(EmptyTokenError) as e:
+            os.remove('../token_save.pkl')
+            m = MockAuthToken(test_mode=True)
+            m.fail_mode = True
+            token = m.get_auth_token('abc', 'def', 'abc123')
 
 
+
+class MockAuthToken(Main):
+
+    def __init__(self, test_mode=True):
+        super().__init__(test_mode)
+        self.fail_mode = False
+
+    def _sb_api_query(self, path: str, function: str, params: Tuple[Any], fail_counter: int = 0):
+        if self.fail_mode:
+            return {'result': ''} # Is this really what a bad result looks like?
+        else:
+            return {'result':'new_token_res'}
